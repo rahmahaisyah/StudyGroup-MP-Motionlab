1a605419f316afaf90d4c9703eb205d752461383
rahmahaisyah <rahmahasyh@gmail.com>
2024-12-11 00:21:14 +0700

    task 3

---
+++ lib/utils/app_style.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class Style {
+  static TextStyle headline = const TextStyle(
+    fontSize: 20,
+    color: Colors.black,
+    fontWeight: FontWeight.bold,
+    fontFamily: 'Roboto',
+  );
+  static TextStyle headline2 = const TextStyle(
+    fontSize: 16,
+    color: Color(0xff707070),
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Roboto',
+  );
+  static TextStyle headline3 = const TextStyle(
+    fontSize: 12,
+    color: Color(0xff00623B),
+    fontWeight: FontWeight.w700,
+    fontFamily: 'Roboto',
+  );
+  static TextStyle headline4 = const TextStyle(
+    fontSize: 2,
+    color: Color(0xff344E41),
+    fontWeight: FontWeight.w100,
+    fontFamily: 'Roboto',
+  );
+  static TextStyle headline5 = const TextStyle(
+    fontSize: 14,
+    color: Colors.black,
+    fontWeight: FontWeight.w200,
+    fontFamily: 'Roboto',
+  );
+}
