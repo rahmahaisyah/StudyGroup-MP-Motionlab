997c54dbe2e531cad6bcece340f5ea4c00764602
rahmahaisyah <rahmahasyh@example.com>
2025-02-10 05:27:04 +0700

    Assignment week 8

---
+++ lib/main.dart
@@ -0,0 +1,215 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'firebase_options.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => MainApp(),
+    ),
+  );
+}
+class MainApp extends StatelessWidget {
+  MainApp({Key? key}) : super(key: key);
+
+  // TextEditingController untuk melakukan controlling input text
+  final _textEditingController = TextEditingController();
+
+  // Fungsi akan dipanggil ketika tombol 'tambah todo' ditekan
+  void handleCreateTodo() {}
+
+  // Fungsi akan dipanggil ketika todo di checklist/unchecklist
+  void handleToggleTodo(String id, bool status) {}
+
+  // Fungsi akan dipanggil ketika menghapus salah satu todo
+  void handleDeleteTodo(String id) {}
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: const Color(0xFFEEEFF5),
+        body: SafeArea(
+          child: Stack(
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                ),
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: ListView(
+                        children: [
+                          const Text(
+                            'List Of Todos',
+                            style: TextStyle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+                          Column(
+                            children: [
+                              // Todo Item
+                              TodoItemWidget(
+                                id: "1",
+                                name: "Ini todo pertama",
+                                status: true,
+                                onDelete: (id) {
+                                  handleDeleteTodo(id);
+                                },
+                                onToggle: (id, status) {
+                                  handleToggleTodo(id, status);
+                                },
+                              ),
+                              TodoItemWidget(
+                                id: "2",
+                                name: "Ini todo kedua",
+                                status: false,
+                                onDelete: (id) {
+                                  handleDeleteTodo(id);
+                                },
+                                onToggle: (id, status) {
+                                  handleToggleTodo(id, status);
+                                },
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Row(children: [
+                  Expanded(
+                    child: Container(
+                      margin: const EdgeInsets.only(
+                        bottom: 20,
+                        right: 20,
+                        left: 20,
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 5,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: TextField(
+                        controller: _textEditingController,
+                        decoration: const InputDecoration(
+                            hintText: 'Add a new todo item',
+                            border: InputBorder.none),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(
+                      bottom: 20,
+                      right: 20,
+                    ),
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.pink,
+                      ),
+                      child: TextButton(
+                        onPressed: () {
+                          handleCreateTodo();
+                        },
+                        child: const Text(
+                          '+',
+                          style: TextStyle(
+                            fontSize: 40,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ]),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TodoItemWidget extends StatelessWidget {
+  final String id;
+  final String name;
+  final bool status;
+  final void Function(String) onDelete;
+  final void Function(String, bool) onToggle;
+
+  const TodoItemWidget({
+    super.key,
+    required this.id,
+    required this.name,
+    required this.status,
+    required this.onDelete,
+    required this.onToggle,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 20),
+      child: ListTile(
+        onTap: () {
+          onToggle(id, status);
+        },
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+        tileColor: Colors.white,
+        leading: Icon(
+          status ? Icons.check_box : Icons.check_box_outline_blank,
+          color: const Color(0xFF5F52EE),
+        ),
+        title: Text(
+          name,
+          style: TextStyle(
+            fontSize: 16,
+            color: const Color(0xFF3A3A3A),
+            decoration: status ? TextDecoration.lineThrough : null,
+          ),
+        ),
+        trailing: Container(
+          padding: const EdgeInsets.all(0),
+          margin: const EdgeInsets.symmetric(vertical: 12),
+          height: 35,
+          width: 35,
+          decoration: BoxDecoration(
+            color: const Color(0xFFDA4040),
+            borderRadius: BorderRadius.circular(5),
+          ),
+          child: IconButton(
+            color: Colors.white,
+            iconSize: 18,
+            icon: const Icon(Icons.delete),
+            onPressed: () {
+              onDelete(id);
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
